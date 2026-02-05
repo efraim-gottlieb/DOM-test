a9f3bce10ef60a9391352d132b9b3d52b9616e67
EfraimGottlieb <efgo583208979@gmail.com>
2026-02-05 16:44:13 +0200

    final

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const elDiceTwo = document.getElementById("dice2");
 const elComeOut = document.getElementById("roll");
 const players = document.querySelector(".players");
 let curPlayer;
+let goalScore = Math.floor(Math.random() * (50 - 50 + 1) + 50);
+const goal = document.querySelector(".goal");
+goal.textContent = `Score: ${goalScore}`;
 const scoreStack = {
   "Player 1": 0,
   "Player 2": 0,
 };
+initRandomPlayer();
+const dict = {};
 function initRandomPlayer() {
   curPlayer =
     Object.keys(scoreStack)[Math.floor(Math.random() * (2 - 0 + 0) + 0)];
 }
 
 diaplayPlayers();
-initRandomPlayer();
 function changePlayer() {
   if (curPlayer == Object.keys(scoreStack)[0]) {
     curPlayer = Object.keys(scoreStack)[1];
@@ -41,7 +45,6 @@ function rollDice() {
       elDiceTwo.classList.add("show-" + k);
     }
   }
-
   const a = elDiceOne.classList[elDiceOne.classList.length - 1];
   const b = elDiceTwo.classList[elDiceTwo.classList.length - 1];
   if (a == b) {
@@ -49,17 +52,26 @@ function rollDice() {
     console.log(curPlayer);
     changePlayer();
   } else {
-     scoreStack[curPlayer] += 5
+    scoreStack[curPlayer] += +a.at(-1) + +b.at(-1);
   }
-  diaplayPlayers()
+  diaplayPlayers();
+  if (scoreStack[curPlayer] >= goalScore) {
+    document.body.innerHTML = "";
+    const won = document.createElement("div");
+    won.className = "won";
+    won.textContent = `${curPlayer} won !!!`;
+    document.body.append(won);
+  }
 }
 
 function diaplayPlayers() {
-  players.innerHTML = ''
+  players.innerHTML = "";
   for (const [player, score] of Object.entries(scoreStack)) {
     const playerDiv = document.createElement("div");
     playerDiv.innerHTML = player;
     playerDiv.id = player;
+    playerDiv.classList.add("player");
+    if (curPlayer == player) playerDiv.classList.add("cur-player");
     const scoreDiv = document.createElement("div");
     scoreDiv.innerHTML = score;
     playerDiv.append(scoreDiv);
@@ -67,3 +79,13 @@ function diaplayPlayers() {
   }
 }
 
+const control = document.querySelector(".game");
+const passButton = document.createElement("button");
+passButton.id = "pass";
+passButton.textContent = "Pass";
+control.append(passButton);
+
+pass.addEventListener("click", () => {
+  changePlayer();
+  diaplayPlayers();
+});
